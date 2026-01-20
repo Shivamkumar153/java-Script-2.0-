8b859f892b2320a9d7eb88189f170b7f4d368a95
Shivamkumar153 <shivamkumarr.1221@gmail.com>
2026-01-20 11:39:17 +0530

    conversion

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,40 @@
+// convert string to number 
+let score = "33afd";
+let data = null;
+
+// console.log(typeof null);
+// console.log(typeof(null));
+
+let valueInNumber = Number(null); // conversion
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "36" => 36;
+// "33asd"=> NaN;
+// null =>0;
+// undefined => NaN;
+//true => 1; , false =>0;
+
+
+let isLoggedIn = "0";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+//console.log(booleanIsLoggedIn);
+
+// 1 => true , 0 => false;
+// "" => false
+// " any things in string" => true ;
+
+
+// convert number to string
+
+let marks = 54;
+let marksInStrig = String(marks);
+// console.log(marksInStrig);
+// console.log(typeof marksInStrig);
+
+let value = NaN;
+let valueInNumber2 = Number(value);
+console.log(valueInNumber2);
+console.log(typeof valueInNumber2);
