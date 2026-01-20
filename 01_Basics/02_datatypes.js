259fb86b36d921397a5303928d4aac31c53a330c
Shivamkumar153 <shivamkumarr.1221@gmail.com>
2026-01-20 11:06:45 +0530

    null datatype is object

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; // treat all js code as newer version 
+
+// alert (8+6) // we are using node js , not browser 
+
+ // datatypes 
+ // number => 2 to power 53
+ // bigint ;
+ // string => ""
+ // boolean => true/false
+ // null => standalone value 
+// undefined => no value assigned;
+// symbol => unique 
+
+
+// object => 
+
+// console.log(typeof undefined);
+// console.log(typeof null); // bug in js => object;
