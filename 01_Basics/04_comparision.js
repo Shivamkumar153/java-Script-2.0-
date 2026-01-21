3fa9776dfbe42ed903ce3f8f88acda5518e8ef80
Shivamkumar153 <shivamkumarr.1221@gmail.com>
2026-01-21 23:31:06 +0530

    string

--- 01_Basics/04_comparision.js
+++ 01_Basics/04_comparision.js
@@ -35,28 +35,59 @@
 // Array, object, Function;
 
 // arrays :-
-  const friends = ["aman","prince", "gullu"];
+  //const friends = ["aman","prince", "gullu"];
   // objects:-
-let myObj = {
-    name : "shivam",
-    age : 22,
-    marks : 90.0,
-}
+// let myObj = {
+//     name : "shivam",
+//     age : 22,
+//     marks : 90.0,
+// }
 
 //function:-
-function school(){
-    let objStudent ={
-        name : "xyz",
-        age : 12,
-        grade : "b"
-    }
-    console.log(objStudent);
-}
-return school();
+// function school(){
+//     let objStudent ={
+//         name : "xyz",
+//         age : 12,
+//         grade : "b"
+//     }
+//     console.log(objStudent);
+// }
+// return school();
 
 
+// ***********************************************************************************
+
+//Stack , Heap ;
+
+// STack(Primitive) , Heap(Non-Ptimitive);
+
+// let num1 = 6;
+// num2 = 9;
+// let num3 = num2 == num1;
+// console.log(num3);
+// console.log(num1);
+// console.log(num2);
 
 
+//object 
+// let princeOld = {
+//      name : "prince",
+//      age : 23,
+//      marks :98,
+//      email : "prince@123gmail.com",
 
+// }
+// let princeNew = princeOld
+//  princeOld.email = "prince@12112gmail.com";
+
+// console.log(princeNew.email);
+// console.log(princeOld.email);
+
+// let amanOld ={
+//     name : "Aman",
+//      age : 24,
+//      marks :88,
+//      email : "aman@113gmail.com",
+// }
 
 
---
+++ 01_Basics/05_string.js
@@ -0,0 +1,10 @@
+// the new why to concat string is 
+const name = "shivam";
+const repoCount = 12;
+
+// old way 
+console.log(name + repoCount + "");
+
+// new way 
+
+console.log(`.....${name}.... ${repoCount}...`);
