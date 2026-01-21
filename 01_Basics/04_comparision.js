50d137170e1f5a14a3ce6220362ef1ad727d6269
Shivamkumar153 <shivamkumarr.1221@gmail.com>
2026-01-21 22:12:55 +0530

    all datatype and function

---
+++ 01_Basics/04_comparision.js
@@ -0,0 +1,62 @@
+// try to compare the same data type;
+
+// console.log(null >0);
+// console.log(null >=0);
+// console.log(null==0);
+
+
+// == -> its only checks value 
+// === -> its checks value and data type both
+
+// kiss terha say memery ko store kerte hai or kiss terha say access kerte hai us basics p hum log apne data 
+ // type ko categorize ma rhakte hai
+
+// 1. Primitive data type       
+// 2. Non-Primitive data type (reference data type)
+
+// Primitive data type :- call  by value (matlb )
+// 7 types :- String, NUmber,Boolean, null, undefined,
+//symbol,BigInt
+
+
+// Reference(Non - primitive data type)(call by reference);
+
+//javaScript is a dynamically typed language means its not mandatory to define data type
+
+
+
+
+// const id = Symbol('123'); // in symbol if your value is same then also its consider as different value;
+// const id2 = Symbol('123');
+// console.log(id==id2);
+
+
+
+// Array, object, Function;
+
+// arrays :-
+  const friends = ["aman","prince", "gullu"];
+  // objects:-
+let myObj = {
+    name : "shivam",
+    age : 22,
+    marks : 90.0,
+}
+
+//function:-
+function school(){
+    let objStudent ={
+        name : "xyz",
+        age : 12,
+        grade : "b"
+    }
+    console.log(objStudent);
+}
+return school();
+
+
+
+
+
+
+
