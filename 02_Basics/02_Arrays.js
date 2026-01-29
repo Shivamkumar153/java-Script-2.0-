a25c99198cfd4b3abfa9b39182514e044f20a621
Shivamkumar153 <shivamkumarr.1221@gmail.com>
2026-01-30 00:10:13 +0530

    arrays function

---
+++ 02_Basics/02_Arrays.js
@@ -0,0 +1,16 @@
+const Even_Numbers = [2,4,6,8,10];
+const Odd_Numbers = [3,5,7,9,11];
+
+// new_Number = Even_Numbers.push(Odd_Numbers); // we can put these things in new variable beacuse it will change it in same arrays it will not copys it; 
+// console.log(new_Number);
+// console.log(new_Number[5]);
+
+//Even_Numbers.push(Odd_Numbers); // it will give right answer ;
+//console.log(Even_Numbers);
+
+// there is another way to add two arrays;
+// const newArr = [...Even_Numbers, ...Odd_Numbers];// spread operator;
+// console.log(newArr);
+// console.log(newArr[6]);
+
+
