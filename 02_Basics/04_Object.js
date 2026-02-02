9d76cbb74369845215b2a02631424ccedbdb8779
Shivamkumar153 <shivamkumarr.1221@gmail.com>
2026-02-03 00:00:04 +0530

    object

---
+++ 02_Basics/04_Object.js
@@ -0,0 +1,20 @@
+// const tinderUser = new tinderUser()
+const tinderUser ={}
+tinderUser.id = "123ABC"
+tinderUser.Name = "shivam"
+tinderUser.isLoggedIn = false
+
+//console.log(tinderUser);
+
+
+const myName = {
+    email:"shivam@123gmail.com",
+    myName: {
+        fatherName :{
+            HisName :"shivam"
+        }
+    }
+}
+//console.log (myName.myName.fatherName.HisName)
+
+ 
